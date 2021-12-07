231adb23716eed2347f85bc1ebbebce5d83f4fb1
liudoudou86 <543972930@qq.com>
2021-12-07 12:36:53 +0800

    update coding

--- Spider_Static.py
+++ Spider_Static.py
@@ -14,25 +14,20 @@ def gethtml():
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34"}
     try:
         txt = ''
-        jump = ''
         res = requests.get(url, headers) # 爬取网页
         soup = BeautifulSoup(res.text, 'lxml') # 使用bs4对网页进行分析
         result = soup.select('.c-single-text-ellipsis') # 对关键词进行class的定位\
-        link = soup.select('.img-wrapper_29V76')
         for i in range(0,10):
             results = result[i].text.strip().replace('\n', '')
-            links = link[i]['href']
             txt += '【' + str(i+1) + '】'
             txt += results
             txt += '\n\n'
-            jump += links
-            jump += '\n\n'
         '''
         with open('D:/Coding/数据.txt', mode= 'w', encoding= 'utf-8') as file:
             file.write(txt)
         '''
-        # print(jump)
-        return txt, jump
+        # print(txt)
+        return txt
     except:
         print("炸了！")
 
@@ -43,7 +38,7 @@ def send_msg(msg):
     try:
         url = "https://oapi.dingtalk.com/robot/send?access_token=7050f5a5e20958eed4aaa270ea19376e700e0cac0aa8768b8be1566a12980a51"
         dd = DingDing(webhook = url)
-        info = dd.Send_MardDown_Msg(Title= '百度热搜', Content= '### **百度热搜：**\n' + msg[0] + msg[1], isAtAll= True)
+        info = dd.Send_MardDown_Msg(Title= '百度热搜', Content= '### **百度热搜：**\n' + msg, isAtAll= True)
         print(info)
     except:
         print("没发出去~")
